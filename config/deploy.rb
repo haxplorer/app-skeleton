69cbb5f7eb4c7039aa24e0a0ad412e0fb791b9ba
Rajagopal Natarajan <rajagopal.n@gmail.com>
2017-07-28 20:21:39 +0530

    Set up Puma, Nginx & Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # Change these
-server 'your_server_ip', port: your_port_num, roles: [:web, :app, :db], primary: true
+server 'greengunny-dev', port: 80, roles: [:web, :app, :db], primary: true
 
-set :repo_url,        'git@example.com:username/appname.git'
-set :application,     'appname'
+set :repo_url,        'git@github.com:haxplorer/app-skeleton.git'
+set :application,     'app-skeleton'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
